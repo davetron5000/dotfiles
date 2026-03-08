556e768e28c42d8232045c93a3316a5edd1eb8f6
Dave Copeland <davec@naildrivin5.com>
2026-03-08 14:55:38 -0400

    use git-delta

--- linux-setup/0500-git.sh
+++ linux-setup/0500-git.sh
@@ -7,3 +7,10 @@ git config --global user.name "Dave Copeland"
 git config --global user.email "davec@naildrivin5.com"
 git config --global init.defaultBranch main
 
+sudo apt install git-delta
+git config --global core.pager delta
+git config --global delta.navigate true
+git config --global delta.line-numbers true
+git config --global interactive.diffFilter "delta --color-only"
+
+
