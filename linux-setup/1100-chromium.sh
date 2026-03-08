52cf70232cd4cf315e90aa81cdd0e34e18ebbf32
Dave Copeland <davec@naildrivin5.com>
2026-03-08 14:55:43 -0400

    tell chromium to fuck off

--- linux-setup/1100-chromium.sh
+++ linux-setup/1100-chromium.sh
@@ -6,4 +6,4 @@ sudo apt update
 sudo apt remove --yes chromium-browser
 sudo apt remove --yes chromium
 flatpak install flathub org.chromium.Chromium
-
+xdg-settings set default-web-browser firefox.desktop
