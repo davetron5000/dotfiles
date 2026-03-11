e4e037e535a1db287b92a1f6d3962a4414683435
Dave Copeland <davec@naildrivin5.com>
2026-03-11 10:16:51 -0400

    fuck snap. just fuck it for ever.

--- linux-setup/0130-firefox.sh
+++ linux-setup/0130-firefox.sh
@@ -5,10 +5,13 @@ set -e
 sudo snap remove firefox
 sudo add-apt-repository ppa:mozillateam/ppa
 sudo apt update
-echo '
-Package: *
+echo 'Package: *
 Pin: release o=LP-PPA-mozillateam
 Pin-Priority: 1001
+
+Package: firefox*
+Pin: release o=Ubuntu
+Pin-Priority: -1
 ' | sudo tee /etc/apt/preferences.d/mozilla-firefox
 sudo apt install firefox
 echo "You gotta reboot because this is bullshit"
